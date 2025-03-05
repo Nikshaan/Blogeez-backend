f7a2b8a4248a5fab5b16bf677c148d6e0bad83cf
Nikshaan <shettynikshaan@gmail.com>
2025-03-05 21:45:32 +0530

    update: database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async() => {
-    await mongoose.connect("mongodb+srv://nikshaan:1UCL7768op17hpVe@cluster0.r21rp.mongodb.net/blogeez");
+    await mongoose.connect(process.env.URI);
 };
 
 module.exports = connectDB;
